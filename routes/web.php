b91f3c836e6a03fff631830c1436785e69cfe751
Hamza Alayed <developerh108@gmail.com>
2019-06-23 11:55:22 +0300

    Initial KNet Payment Package

---
+++ routes/web.php
@@ -0,0 +1,6 @@
+<?php
+
+
+Route::get('knet', function(){
+    echo 'Hello from the KNet package!';
+});
---
+++ src/Providers/KNetServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace DeveloperH\KNet;
+
+use Illuminate\Support\ServiceProvider;
+
+class KNetServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->loadRoutesFrom(__DIR__ . '/../routes/web.php');
+    }
+}
